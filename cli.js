b91ae0d1ea76e9d3e4ecf62417e34eb24aa57cda
Jason Smith (air) <jhs@iriscouch.com>
2012-07-15 08:36:17 +0700

    Execute the FastCGI daemon and run a no-op http server

--- cli.js
+++ cli.js
@@ -3,7 +3,9 @@
 // FastCGI web server
 
 var util = require('util')
+var http = require('http')
 var optimist = require('optimist')
+var child_process = require('child_process')
 
 var fastcgi = require('./fastcgi')
 
@@ -17,7 +19,45 @@ function main() {
   if(ARGV.help)
     return usage()
 
-  console.log('Done.')
+  var command = ARGV._[0]
+    , args    = ARGV._.slice(1)
+    , options = {}
+
+  console.log('Run: %j %j', command, args)
+  var child = child_process.spawn(command, args, options)
+
+  child.stdout.on('data', function(data) {
+    console.log('STDOUT: %j', data)
+  })
+
+  child.stderr.on('data', function(data) {
+    if (/^execvp\(\)/.test(data))
+      return console.error('Failed to start child process')
+
+    console.log('STDERR: %j', data)
+  })
+
+  child.on('exit', function(code) {
+    console.log('Exit %j: %d', command, code)
+  })
+
+  // Now run the HTTP front-end.
+  httpd()
+}
+
+function httpd() {
+  var host = '0.0.0.0'
+    , port = ARGV.port
+    , fcgi_service = ARGV.socket
+
+  var server = http.createServer(on_req)
+  server.listen(port, host)
+  console.log('HTTP on %s:%d', host, port)
+
+  function on_req(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+  }
 }
 
 //
@@ -26,14 +66,15 @@ function main() {
 
 function get_argv() {
   OPTS = optimist.boolean(['die'])
-                 .demand(['port'])
+                 .demand(['port', 'socket'])
                  .default({ 'max': 25
                          })
                  .describe({ 'die': 'Exit after serving one request'
                            , 'port': 'Listening port number'
                            , 'max': 'Maximum allowed subprocesses'
+                           , 'socket': 'Unix socket FastCGI program will use'
                           })
-                 .usage('Usage: $0 [options] <FastCGI program>')
+                 .usage('Usage: $0 [options] <FastCGI program> [program arg1] [arg2] [...]')
 
   ARGV = OPTS.argv
 }
