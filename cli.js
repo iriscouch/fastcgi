746595a581941086d8118c6ce331ba514ae7f01e
Jason Smith (air) <jhs@iriscouch.com>
2012-07-23 11:04:54 +0700

    Reimplement daemonization using Node.js v0.8 child_process to re-execute myself

--- cli.js
+++ cli.js
@@ -4,7 +4,7 @@
 
 var fs = require('fs')
 var util = require('util')
-var daemon = require('daemon')
+var path = require('path')
 var optimist = require('optimist')
 var child_process = require('child_process')
 
@@ -12,9 +12,6 @@ var fastcgi = require('./fastcgi')
 
 var LOG = console
 var ARGV = null, OPTS = null
-
-var IS_FIRST_TICK = true
-process.nextTick(function() { IS_FIRST_TICK = false })
 
 if(require.main === module)
   main(get_argv())
@@ -24,62 +21,76 @@ function main() {
     return usage()
 
   if(ARGV.daemon && !ARGV.log)
-    return usage()
+    return usage('log')
+  if(ARGV.daemon && !ARGV.pidfile)
+    return usage('pidfile')
+
+  if(ARGV.daemon)
+    return daemonize()
+  else if(ARGV.parent)
+    childize(init)
+  else
+    init()
+}
+
+function init(er) {
+  if(er)
+    throw er
+
+  process.on('SIGINT', die)
+  process.on('SIGTERM', die)
 
   var command = ARGV._[0]
     , args    = ARGV._.slice(1)
     , options = {}
 
-  if(ARGV.daemon)
-    daemonize()
-
   if(!command)
-    begin_http()
-  else {
-    var http_timer = setTimeout(begin_http, 250)
+    return begin_http()
 
-    LOG.log('Run: %j %j', command, args)
-    var child = child_process.spawn(command, args, options)
+  var http_timer = setTimeout(begin_http, 250)
 
-    child.stderr.setEncoding('utf8')
-    child.stdout.setEncoding('utf8')
+  LOG.log('Run: %j %j', command, args)
+  var child = child_process.spawn(command, args, options)
 
-    child.stdout.on('data', function(data) {
-      data.split(/\n/).forEach(function(line) {
-        LOG.log('STDOUT: %s', line)
-      })
+  child.stderr.setEncoding('utf8')
+  child.stdout.setEncoding('utf8')
+
+  child.stdout.on('data', function(data) {
+    data.split(/\n/).forEach(function(line) {
+      LOG.log('STDOUT: %s', line)
     })
+  })
 
-    child.stderr.on('data', function(data) {
-      if (/^execvp\(\)/.test(data))
-        return LOG.error('Failed to start child process')
+  child.stderr.on('data', function(data) {
+    if (/^execvp\(\)/.test(data))
+      return LOG.error('Failed to start child process')
 
-      data.split(/\n/).forEach(function(line) {
-        LOG.log('STDERR: %s', line)
-      })
+    data.split(/\n/).forEach(function(line) {
+      LOG.log('STDERR: %s', line)
     })
+  })
 
-    child.on('exit', function(code) {
-      clearTimeout(http_timer)
-      LOG.log('Exit %j: %d', command, code)
-    })
+  child.on('exit', function(code) {
+    clearTimeout(http_timer)
+    LOG.log('Exit %j: %d', command, code)
+  })
 
-    process.on('SIGINT', die)
-    process.on('SIGTERM', die)
-    function die() {
-      LOG.info('Exit; killing child at %d', child.pid)
+  function die() {
+    LOG.log('Exit')
+    if(child) {
+      LOG.info('Kill child at %d', child.pid)
       child.kill('SIGTERM')
+    }
 
-      if(ARGV.daemon)
-        undaemonize(finished)
-      else
-        finished()
+    if(ARGV.parent)
+      undaemonize(finished)
+    else
+      finished()
 
-      function finished(er) {
-        if(er)
-          throw er
-        process.exit(0)
-      }
+    function finished(er) {
+      if(er)
+        throw er
+      process.exit(0)
     }
   }
 
@@ -95,31 +106,101 @@ function main() {
 }
 
 function daemonize() {
-  if(! IS_FIRST_TICK)
-    throw new Error('Too late to daemonize Node.js')
+  LOG.info('Daemonize, log at %s; pid at %s', ARGV.log, ARGV.pidfile)
 
-  var message = 'Daemonize, log at ' + ARGV.log
-  if(ARGV.lock)
-    message += '; lock at ' + ARGV.lock
-  LOG.info(message)
+  // Do a poor-man's exclusive open of the PID file, while the console is still conveniently available.
+  var pidfile = fs.createReadStream(ARGV.pidfile, {'encoding':'utf8'})
 
-  var pid = daemon.start(ARGV.log)
-  LOG.log('pid is %d', pid)
+  pidfile.on('open', function(fd) {
+    // Daemonization cannot continue with a PID file in place.
+    var data = ''
+    pidfile.on('end', done)
+    pidfile.on('error', function(er) { throw er })
+    pidfile.on('data', function(chunk) {
+      data += chunk
+      if(data.length > 10)
+        done()
+    })
 
-  if(ARGV.lock) {
-    var is_locked = daemon.lock(ARGV.lock)
-    if(!is_locked)
-      throw new Error('Failed to lock file: ' + ARGV.lock)
-  }
+    function done() {
+      var pid = +data
+      if(typeof pid != 'number' || isNaN(pid))
+        pid = '[unknown]'
+
+      LOG.error('Daemon already running at pid %s', pid)
+      process.exit(1)
+    }
+  })
+
+  pidfile.on('error', function(er) {
+    if(er.code != 'ENOENT')
+      throw er
+
+    // Good. Open the log file and spawn the child. (The child will try an exclusive open too of course.)
+    // Also, it seems as if the child needs two distinct file descriptors. Not sure if that is a bug.
+    var log = fs.createWriteStream(ARGV.log, {'flags':'a', 'mode':0600})
+    log.on('error', function(er) { throw er })
+
+    log.on('open', function(out_fd) {
+      var err = fs.createWriteStream(ARGV.log, {'flags':'a', 'mode':0600})
+      err.on('error', function(er) { throw er })
+
+      err.on('open', function(err_fd) {
+        log.write(util.format('%d: Spawn daemon\n', process.pid))
+        spawn(out_fd, err_fd)
+      })
+    })
+  })
+}
+
+function spawn(new_stdout, new_stderr) {
+  var command = process.argv[0]
+    , opts = {'cwd':'/', 'detached':true, 'stdio':['ignore', new_stdout, new_stderr||new_stdout]}
+
+  var args = process.argv.slice(1)
+    .map(function(arg) {
+      if(arg.match(/^--daemon/))
+        return '--parent=' + process.pid
+      var match = arg.match(/^--(\w+)=(.*)$/)
+        , key = match && match[1]
+        , val = match && match[2]
+      if(key == 'log')
+        return null
+      if(key == 'socket' || key == 'pidfile')
+        return '--' + key + '=' + path.resolve(val)
+      return arg
+    })
+    .filter(function(arg) { return !! arg })
+
+  //LOG.log('Parent: %d\ncommand: %j\nargs: %j\nopts: %j', process.pid, command, args, opts)
+
+  var child = child_process.spawn(command, args, opts)
+  child.unref()
+}
+
+function childize(callback) {
+  // Initialize the daemon child.
+  var pidfile = fs.createWriteStream(ARGV.pidfile, {'flags':'wx', 'mode':0640, 'encoding':'utf8'})
+  pidfile.on('error', function(er) {
+    callback(er)
+  })
+
+  pidfile.on('open', function(fd) {
+    pidfile.write(process.pid + '\n')
+    pidfile.end()
+
+    LOG.info('%d: daemon running', process.pid)
+    callback()
+  })
 }
 
 function undaemonize(callback) {
-  if(ARGV.lock)
-    fs.unlink(ARGV.lock, function(er) {
+  if(ARGV.pidfile)
+    fs.unlink(ARGV.pidfile, function(er) {
       if(er)
-        LOG.error('Failed to clean lock file %j: %s', ARGV.lock, er.message)
+        LOG.error('Failed to clean PID file %j: %s', ARGV.pidfile, er.message)
       else
-        LOG.log('Cleaned pid file: %s', ARGV.lock)
+        LOG.log('Cleaned pid file: %s', ARGV.pidfile)
 
       return callback(er)
     })
@@ -138,8 +219,8 @@ function get_argv() {
                            , 'log': 'Path to log file'
                            , 'port': 'Listening port number'
                            , 'max': 'Maximum allowed subprocesses'
-                           , 'daemon': 'Daemonize (run in the background); requires --log'
-                           , 'lock': 'Lockfile to use when daemonizing'
+                           , 'daemon': 'Daemonize (run in the background); requires --log and --pidfile'
+                           , 'pidfile': 'Lockfile to use when daemonizing'
                            , 'socket': 'Unix socket FastCGI program will use'
                           })
                  .usage('Usage: $0 [options] <FastCGI program> [program arg1] [arg2] [...]')
